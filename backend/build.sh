2dd4002543f00b106ef9d28e2a40c86365b987b3
fedev254 <felixabel772@gmail.com>
2025-08-20 18:32:58 +0300

    fix(build): Correct order of commands in build script

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
+# STEP 1: Install all project dependencies from requirements.txt
 pip install -r requirements.txt
 
+# STEP 2: Now that Django is installed, collect the static files for the admin panel.
 python manage.py collectstatic --no-input
+
+# STEP 3: And now, apply any database migrations.
 python manage.py migrate
